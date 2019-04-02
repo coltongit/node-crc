61d81886a30497b6e92f8c74bd723b6453f60cb2
Colton Rushton <colton51919@gmail.com>
2019-04-02 14:11:48 -0300

    Modified mocha.opts and package.json to fix deprecation warnings and allow Greenkeeper to update Mocha, as well as added verification to CRC5 and CRC12-3GPP in module-import.test.js.

--- test/module-import.test.js
+++ test/module-import.test.js
@@ -1,6 +1,15 @@
 import chai from 'chai';
 
-import { crc32 as crc32mod, crc16kermit as crc16kermit_mod } from '..';
+import {
+  crc5 as crc5mod,
+  crc123gpp as crc123gpp_mod,
+  crc32 as crc32mod,
+  crc16kermit as crc16kermit_mod
+} from '..';
+import crc5src from '../crc5';
+import crc5lib from '../lib/es6/crc5';
+import crc123gpp_src from '../crc123gpp';
+import crc123gpp_lib from '../lib/es6/crc123gpp';
 import crc32src from '../crc32';
 import crc32lib from '../lib/es6/crc32';
 import crc16kermit_src from '../crc16kermit';
@@ -16,6 +25,12 @@ describe('Module imports', () => {
     it('crc16kermit', () => {
       crc16kermit_mod('1234567890').should.equal(crc16kermit_src('1234567890'));
     });
+    it('crc123gpp', () => {
+      crc123gpp_mod('1234567890').should.equal(crc123gpp_src('1234567890'));
+    });
+    it('crc5', () => {
+      crc5mod('1234567890').should.equal(crc5src('1234567890'));
+    });
   });
 
   describe("Transpiled functions from 'crc/lib/foo' behave the same as module functions", () => {
@@ -24,6 +39,12 @@ describe('Module imports', () => {
     });
     it('crc16kermit', () => {
       crc16kermit_mod('1234567890').should.equal(crc16kermit_lib('1234567890'));
+    });
+    it('crc123gpp', () => {
+      crc123gpp_mod('1234567890').should.equal(crc123gpp_lib('1234567890'));
+    });
+    it('crc5', () => {
+      crc5mod('1234567890').should.equal(crc5lib('1234567890'));
     });
   });
 });
