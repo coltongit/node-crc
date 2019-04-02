93477ecb0c0a70b07015139af8d63223af03b4e5
Colton Rushton <colton51919@gmail.com>
2019-04-02 12:49:32 -0300

    Fixed a build-breaking bug with the CRC12-3GPP test file.

--- test/crc123gpp.test.js
+++ test/crc123gpp.test.js
@@ -1,5 +1,5 @@
 import crcSuiteFor from './test_helpers';
-import crc81wire from '../lib/es6/crc123gpp';
+import crc123gpp from '../lib/es6/crc123gpp';
 
 describe('CRC12 3GPP', function() {
   crcSuiteFor({ crc: crc123gpp });
