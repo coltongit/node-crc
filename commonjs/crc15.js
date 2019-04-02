36faf37af38f481fe4dc1c115d55b66bf71dcd73
Colton Rushton <colton51919@gmail.com>
2019-04-02 12:21:39 -0300

    Added CRC5, CRC12-3GPP, and CRC15, along with adding preparations to re-add Greenkeeper.

---
+++ commonjs/crc15.js
@@ -0,0 +1,1 @@
+module.exports = require('./es6/crc15').default;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import crc1 from './crc1';
+import crc5 from './crc5';
 import crc8 from './crc8';
 import crc81wire from './crc81wire';
+import crc123gpp from './crc123gpp';
+import crc15 from './crc15';
 import crc16 from './crc16';
 import crc16ccitt from './crc16ccitt';
 import crc16modbus from './crc16modbus';
@@ -8,11 +11,15 @@ import crc16xmodem from './crc16xmodem';
 import crc16kermit from './crc16kermit';
 import crc24 from './crc24';
 import crc32 from './crc32';
+//import crc64 from './crc64';
 import crcjam from './crcjam';
 
 export { crc1 };
+export { crc5 };
 export { crc8 };
 export { crc81wire };
+export { crc123gpp };
+export { crc15 };
 export { crc16 };
 export { crc16ccitt };
 export { crc16modbus };
@@ -20,12 +27,16 @@ export { crc16xmodem };
 export { crc16kermit };
 export { crc24 };
 export { crc32 };
+//export { crc64 };
 export { crcjam };
 
 export default {
   crc1,
+  crc5,
   crc8,
   crc81wire,
+  crc123gpp,
+  crc15,
   crc16,
   crc16ccitt,
   crc16modbus,
@@ -33,5 +44,6 @@ export default {
   crc16kermit,
   crc24,
   crc32,
+//crc64,
   crcjam,
 };
